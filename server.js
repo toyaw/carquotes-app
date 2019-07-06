2115249ae3d53dad13aac2b829782e9b8ff39285
Toya Williams <toyaw7@gmail.com>
2019-07-06 15:58:28 -0400

    adding index

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load()
+  require('dotenv').config();
 }
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routers/index')
+const authorRouter = require('./routers/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -20,5 +21,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+'authors/new'
 
 app.listen(process.env.PORT || 3000)
