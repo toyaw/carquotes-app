f47b4433b7bd69edfa46904ac8a9cad597c81799
Toya Williams <toyaw7@gmail.com>
2019-07-06 19:09:00 -0400

    added body-parser

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routers/index')
 const authorRouter = require('./routers/authors')
-
+const bodyParser = require('body-parser')
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
