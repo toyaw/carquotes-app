50b159133ce9de2fb2428fd21de013533d8ebe34
Toya Williams <toyaw7@gmail.com>
2019-07-06 09:25:07 -0400

    setting up views

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
 
-
-  
-  const express = require('express')
-  const app = express()
-  const expressLayouts = require('express-ejs-layouts')
-  //const exphbs  = require('express-handlebars')
-
- const controllersRouter = require('./controllers/index.js')
+const indexRouter = require('./routers/index')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -14,6 +10,8 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-app.use('/', controllersRouter)
+
+
+app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000)
