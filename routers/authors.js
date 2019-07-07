e1e25148ee2edbf8828fc859a25a0436edd1abf2
Toya Williams <toyaw7@gmail.com>
2019-07-07 08:04:13 -0400

    add additional routes

--- routers/authors.js
+++ routers/authors.js
@@ -3,9 +3,15 @@ const router = express.Router()
 const Author = require('../models/author')
 //All service
 router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
   try {
-    const authors = await Author.find({})
-    res.render('authors/index', { authors: authors})
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', { 
+      authors: authors,
+       searchOptions: req.query})
   } catch {
     res.redirect('/')
   }
